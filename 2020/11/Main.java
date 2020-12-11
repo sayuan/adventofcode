6b0ed2ca22b26b3cf82353e12f813a5edee3f3ad
sayuan <shiao.an.yuan@gmail.com>
2020-12-11 22:56:23 +0800

    Automatic update

--- 2020/11/Main.java
+++ 2020/11/Main.java
@@ -7,31 +7,67 @@ import static java.lang.Math.*;
 import static java.lang.System.*;
 public class Main {
 
+    // → ↘ ↓ ↙ ← ↖ ↑ ↗
+    public static final int[] VR = {0, 1, 1, 1, 0, -1, -1, -1};
+    public static final int[] VC = {1, 1, 0, -1, -1, -1, 0, 1};
+
     public Main() throws Exception {
-        int[][] m = new int[100][100];
+        char[][] m = new char[100][100];
+        Arrays.fill(m[0], 'L');
+        Arrays.fill(m[99], 'L');
+        for (int r=1; r<99; r++) {
+            Arrays.fill(m[r], '.');
+            m[r][0] = 'L';
+            m[r][99] = 'L';
+        }
         for (int r=1; in.hasNext(); r++) {
             String line = in.next();
-            for (int c=0; c<line.length(); c++) m[r][c+1] = line.charAt(c)=='.'?0:1;
+            for (int c=0; c<line.length(); c++) m[r][c+1] = line.charAt(c);
         }
 
+        int q= 0;
         while (true) {
-            int[][] n = new int[100][100];
+            char[][] n = new char[100][];
+            for (int i=0; i<100; i++) n[i] = m[i].clone();
+
+            int[][] cs = new int[100][100];
+            // for (int r=1; r<=98; r++) {
+                // for (int c=1; c<=98; c++) {
+                    // if (m[r][c]!='#') continue;
+                    // for (int i=0; i<8; i++) cs[r+VR[i]][c+VC[i]]++;
+                // }
+            // }
+
+            for (int i=0; i<4; i++) {
+                char[][] t = new char[100][];
+                for (int j=0; j<100; j++) t[j] = m[j].clone();
+                for (int r=98; r>=1; r--) {
+                    for (int c=98; c>=1; c--) {
+                        if (t[r+VR[i]][c+VC[i]]=='#') cs[r][c]++;
+                        if (t[r][c]=='.') t[r][c] = t[r+VR[i]][c+VC[i]];
+                    }
+                }
+            }
+            for (int i=4; i<8; i++) {
+                char[][] t = new char[100][];
+                for (int j=0; j<100; j++) t[j] = m[j].clone();
+                for (int r=1; r<=98; r++) {
+                    for (int c=1; c<=98; c++) {
+                        if (t[r+VR[i]][c+VC[i]]=='#') cs[r][c]++;
+                        if (t[r][c]=='.') t[r][c] = t[r+VR[i]][c+VC[i]];
+                    }
+                }
+            }
+
             boolean changed = false;
             for (int r=1; r<=98; r++) {
                 for (int c=1; c<=98; c++) {
-                    int o = 0;
-                    for (int dr=-1; dr<=1; dr++) {
-                        for (int dc=-1; dc<=1; dc++) {
-                            if (dr==0&&dc==0) continue;
-                            if (m[r+dr][c+dc] == 2) o++;
-                        }
-                    }
-                    n[r][c] = m[r][c];
-                    if (m[r][c]==1&&o==0) {
-                        n[r][c] = 2;
+                    if (m[r][c]=='L'&&cs[r][c]==0) {
+                        n[r][c] = '#';
                         changed = true;
-                    } else if (m[r][c]==2&&o>=4) {
-                        n[r][c] = 1;
+                    // } else if (m[r][c]=='#'&&cs[r][c]>=4) {
+                    } else if (m[r][c]=='#'&&cs[r][c]>=5) {
+                        n[r][c] = 'L';
                         changed = true;
                     }
                 }
@@ -40,7 +76,7 @@ public class Main {
             m = n;
         }
         int ans = 0;
-        for (int r=1; r<=98; r++) for (int c=1; c<=98; c++) if (m[r][c]==2) ans++;
+        for (int r=1; r<=98; r++) for (int c=1; c<=98; c++) if (m[r][c]=='#') ans++;
         System.out.println(ans);
     }
 
